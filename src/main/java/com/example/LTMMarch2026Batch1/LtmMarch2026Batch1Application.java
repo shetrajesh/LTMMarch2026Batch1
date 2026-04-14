874d289f55388f1621c1663a1540d4f81033e62d
rshet007@yahoo.com <rshet007@yahoo.com>
2026-04-14 12:08:47 +0530

    This is the first commit of the project

---
+++ src/main/java/com/example/LTMMarch2026Batch1/LtmMarch2026Batch1Application.java
@@ -0,0 +1,13 @@
+package com.example.LTMMarch2026Batch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtmMarch2026Batch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtmMarch2026Batch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LTMMarch2026Batch1/LtmMarch2026Batch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LTMMarch2026Batch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtmMarch2026Batch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
